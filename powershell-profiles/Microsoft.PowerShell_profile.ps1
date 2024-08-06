43057dec84760d418e0e6de3e55915ba904555eb
Andre Kakoo Brioso <akbrioso@iseg.ulisboa.pt>
2024-08-06 10:42:13 +0100

    Update PowerShell and VSCode profiles with new Oh My Posh theme configuration

--- powershell-profiles/Microsoft.PowerShell_profile.ps1
+++ powershell-profiles/Microsoft.PowerShell_profile.ps1
@@ -1,30 +1,14 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
 #$env:POSH_THEMES_PATH = "C:\Users\AndréKakooBrioso\AppData\Local\Programs\oh-my-posh\themes"
 #oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
 
 
-# Import the Posh-Git module
-#Import-Module -Name Posh-Git
-
-# Import the PSReadLine module
-#Import-Module -Name PSReadLine
 
 
 # Import Terminal-Icons module
 Import-Module -Name Terminal-Icons
 
-# Customize your prompt by modifying the $DefaultUser variable
-$DefaultUser = "akbrioso"
 
-# Add any additional customizations or aliases here
-
-# Set the prompt
-function prompt {
-    $prompt = Get-PoshPrompt
-    $prompt.User = $DefaultUser
-    $prompt.BeforeText = " "
-    $prompt.AfterText = " "
-    $prompt
-}
-
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\half-life.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\half-life.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
