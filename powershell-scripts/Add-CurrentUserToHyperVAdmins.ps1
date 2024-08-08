e99b7a96c907b27b313a404191a3570276fb07c2
Andre Kakoo Brioso <akbrioso@iseg.ulisboa.pt>
2024-08-08 23:22:38 +0100

    Update DSC to add current user to Hyper-V Administrators group Script to add user to Hyper-V Administrators corrected to use SID instead of Name (due to different localization settings)

--- powershell-scripts/Add-CurrentUserToHyperVAdmins.ps1
+++ powershell-scripts/Add-CurrentUserToHyperVAdmins.ps1
@@ -2,7 +2,7 @@
 $currentUserSid = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).User.Value
 
 # Get the Hyper-V Administrators group
-$hyperVAdminsGroup = Get-LocalGroup -Name "Hyper-V Administrators"
+$hyperVAdminsGroup = Get-LocalGroup -SID "S-1-5-32-578"
 
 # Add the current user to the Hyper-V Administrators group
 Add-LocalGroupMember -Group $hyperVAdminsGroup -Member $currentUserSid
