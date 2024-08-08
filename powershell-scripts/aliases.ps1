2969580339e61088d3eef3b4e624c26932b5a2da
Andre Kakoo Brioso <akbrioso@iseg.ulisboa.pt>
2024-08-08 23:50:18 +0100

    feat: Add Test PowerShell aliases and Functions

---
+++ powershell-scripts/aliases.ps1
@@ -0,0 +1,42 @@
+# Easier Navigation: .., ..., ...., ....., and ~
+${function:~} = { Set-Location ~ }
+# PoSh won't allow ${function:..} because of an invalid path error, so...
+${function:Set-ParentLocation} = { Set-Location .. }; Set-Alias ".." Set-ParentLocation
+${function:...} = { Set-Location ..\.. }
+${function:....} = { Set-Location ..\..\.. }
+${function:.....} = { Set-Location ..\..\..\.. }
+${function:......} = { Set-Location ..\..\..\..\.. }
+
+# Navigation Shortcuts
+#${function:drop} = { Set-Location ~\Documents\Dropbox }
+${function:work} = { Set-Location ~\workspace }
+${function:docs} = { Set-Location ~\Documents }
+${function:dl} = { Set-Location ~\Downloads }
+
+# Missing Bash aliases
+Set-Alias time Measure-Command
+
+# Create a new directory and enter it
+Set-Alias mkd CreateAndSet-Directory
+
+# Determine size of a file or total size of a directory
+Set-Alias fs Get-DiskUsage
+
+# Empty the Recycle Bin on all drives
+Set-Alias emptytrash Empty-RecycleBin
+
+# Cleanup old files all drives
+Set-Alias cleandisks Clean-Disks
+
+# Reload the shell
+Set-Alias reload Reload-Powershell
+
+# http://xkcd.com/530/
+Set-Alias mute Set-SoundMute
+Set-Alias unmute Set-SoundUnmute
+
+# Update installed Ruby Gems, NPM, and their installed packages.
+Set-Alias update System-Update
+
+# Set GVim as default vim
+Set-Alias vim gvim
