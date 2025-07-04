b2c0ff5e3228cac0615f81889b8b2f73c4075b9b
Andre Kakoo Brioso <akbrioso@iseg.ulisboa.pt>
2025-07-04 23:29:05 +0100

    Comment out the line to skip running setup scripts for testing purposes

--- setup-scripts/setup.ps1
+++ setup-scripts/setup.ps1
@@ -328,7 +328,7 @@ $setupScripts = Get-ChildItem -Path $DotfilesSetupScriptsFolder -Filter "setup.p
 $setupScripts = $setupScripts | Sort-Object Name
 
 # For testing purposes, you can uncomment the line below to don't run any setup scripts
- $setupScripts = @() # Uncomment this line to skip running setup scripts
+# $setupScripts = @() # Uncomment this line to skip running setup scripts
 
 if (-not $setupScripts) {
     Write-Host "No setup scripts found in $DotfilesSetupScriptsFolder" -ForegroundColor Yellow
